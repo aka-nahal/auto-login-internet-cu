daed794c11c6da284ca50087f1371e438e384051
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2023-04-21 11:53:23 +0530

    Update internet_login.py

--- internet_login.py
+++ internet_login.py
@@ -1,36 +1,44 @@
 import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
 import time
 
-login_url = "http://172.15.15.1:1000/fgtauth"
-username = "YOUR_USERNAME"
-password = "YOUR_PASSWORD"
+# Specify the path to the WebDriver executable
+# For example, for Google Chrome
+webdriver_path = '/path/to/chromedriver'
 
-while True:
-    try:
-        # Get the token number from the login page
-        response = requests.get(login_url)
-        token = response.url.split("?")[-1]
+# Specify the login URL and credentials
+login_url = 'http://172.15.15.1:1000/fgtauth'
+username = 'my_username'
+password = 'my_password'
 
-        # Prepare the form data for the login request
-        data = {
-            "4Tredir": "/fgtauth/",
-            "magic": token,
-            "username": username,
-            "password": password,
-            "submit": "Login",
-        }
+# Create a new session and send a GET request to the login URL
+session = requests.Session()
+response = session.get(login_url)
 
-        # Send the login request
-        response = requests.post(login_url, data=data)
+# Use BeautifulSoup to parse the response and extract the token
+soup = BeautifulSoup(response.content, 'html.parser')
+token = soup.find('input', {'name': 'token'}).get('value')
+login_url_with_token = f'{login_url}?{token}'
 
-        # Check if login was successful
-        if "You have successfully logged in." in response.text:
-            print("Login successful.")
-        else:
-            print("Login failed.")
+# Create a new WebDriver instance and navigate to the login URL with the token
+driver = webdriver.Chrome(webdriver_path)
+driver.get(login_url_with_token)
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+# Wait for the page to load and find the login form elements
+time.sleep(5)  # Adjust as needed
+username_field = driver.find_element_by_name('username')
+password_field = driver.find_element_by_name('password')
+login_button = driver.find_element_by_xpath('//button[@type="submit"]')
 
-    # Wait for 5 hours before attempting to login again
-    time.sleep(5 * 60 * 60)
+# Fill in the login form and submit it
+username_field.send_keys(username)
+password_field.send_keys(password)
+login_button.click()
+
+# Wait for the login to complete and do something else
+time.sleep(10)  # Adjust as needed
+# ... do something else here ...
+
+# Close the browser window
+driver.quit()
